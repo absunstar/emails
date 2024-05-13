284052d5987bdc42c97f8bad95b389fa0ae4e23a
absunstar <absunstar@gmail.com>
2024-05-13 21:05:35 +0300

    #

--- apps/emails/site_files/js/index.js
+++ apps/emails/site_files/js/index.js
@@ -186,4 +186,20 @@ app.controller('emails', function ($scope, $http) {
       }
     );
   };
+
+  function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+
+  $scope.generateEmail = function () {
+    $scope.emailSearch.to = makeid(8) + '@egytag.com';
+  };
 });

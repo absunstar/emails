e60a4bbfc5590d88901e4ec3443413a837826d76
absunstar <absunstar@gmail.com>
2024-05-27 00:42:53 +0300

    #

--- apps/emails/site_files/js/index.js
+++ apps/emails/site_files/js/index.js
@@ -92,8 +92,7 @@ app.controller('emails', function ($scope, $http) {
         if (response.data.done) {
           $scope.currentEmail = response.data.doc;
           $scope.currentEmail.html = $scope.currentEmail.html || $scope.currentEmail.text;
-          document.querySelector('#div-message').innerHTML = $scope.currentEmail.html;
-          document.getElementById('#div-message').src = 'data:text/html;charset=utf-8,' + escape($scope.currentEmail.html);
+          document.querySelector('#div-message').src = 'data:text/html;charset=utf-8,' + escape($scope.currentEmail.html);
         } else {
           $scope.error = response.data.error;
         }

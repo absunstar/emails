2212641cb408906ad59fe02bf2e13ae290d2abd0
absunstar <absunstar@gmail.com>
2024-05-27 00:38:25 +0300

    #

--- apps/emails/site_files/js/index.js
+++ apps/emails/site_files/js/index.js
@@ -92,6 +92,7 @@ app.controller('emails', function ($scope, $http) {
         if (response.data.done) {
           $scope.currentEmail = response.data.doc;
           $scope.currentEmail.html = $scope.currentEmail.html || $scope.currentEmail.text;
+          document.querySelector('#div-message').innerHTML = $scope.currentEmail.html;
         } else {
           $scope.error = response.data.error;
         }
